12d25b1ba67465a1ef31f30c02eb15361f884643
DMouraLobo <diegomouralobo@outlook.com>
2026-05-03 17:11:29 -0300

    Add interface inicial
    Criação do primeiro modelo básico de uma interface para o App

--- projeto_quiz/lib/main.dart
+++ projeto_quiz/lib/main.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(PerguntaApp());
-}
+void main() => runApp(PerguntaApp());
 
 class PerguntaApp extends StatelessWidget {
   const PerguntaApp({super.key});
-
   @override
   Widget build(BuildContext context){
+    final perguntas = [
+      'Qual é a sua cor favorita?',
+      'Qual é o seu animal favorito?',
+    ];
     return MaterialApp(
-      home: Text('Olá Flutter.'),
-
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Perguntas'),
+          centerTitle: true,
+          backgroundColor: Color.fromARGB(120,0,0,255),
+        ),
+        body: Column(
+          children: [
+            Text(perguntas[0]),
+          ],
+        ),
+      ),
     );
   }
 }
