4a062b55204c02a790387e38dc5d4f3194e15d30
DMouraLobo <diegomouralobo@outlook.com>
2026-05-03 18:37:54 -0300

    Update Class Resposta
    Adicionando as funcionalidades e estilização da classe

--- projeto_quiz/lib/resposta.dart
+++ projeto_quiz/lib/resposta.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
 
 class Resposta extends StatelessWidget {
-  const Resposta({super.key});
+
+  final String texto;
+  final void Function() quandoRespondido;
+  Resposta(this.texto, this.quandoRespondido);
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.all(10),
+      child: ElevatedButton(
+        onPressed: quandoRespondido,
+        child: Text(texto),
+        style: ElevatedButton.styleFrom(
+        backgroundColor: Colors.blue,
+        foregroundColor: Colors.white,
+        padding: EdgeInsets.symmetric(vertical: 20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(10))
+      )
+      )
+    );
   }
 }
